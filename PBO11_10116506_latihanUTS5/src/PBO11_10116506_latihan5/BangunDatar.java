0f4ec3689a704d9689679e408ee0c6256584965d
Gilangkw <32380279+Gilangkw@users.noreply.github.com>
2017-11-14 22:38:32 +0700

    Add files via upload

---
+++ PBO11_10116506_latihanUTS5/src/PBO11_10116506_latihan5/BangunDatar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO11_10116506_latihan5;
+
+/**
+ *
+ * @author gilang kusuma wijaya
+ */
+public interface BangunDatar {
+    
+    public void hitungluas();
+    public void hitungkeliling();
+    
+}
